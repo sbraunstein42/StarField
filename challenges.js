ccbf1771cee69562ae19af18bcc561e1f4f3d123
sbraunstein42 <sbraunstein@neit.edu>
2025-10-30 10:27:39 -0400

    Step 2 complete

--- challenges.js
+++ challenges.js
@@ -1,2 +1,11 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+
+function gameLoop() {
+
+}
+
+setInterval(gameLoop, 50);
+
+
+
