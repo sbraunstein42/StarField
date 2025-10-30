b08660d93da13c8d89ef989f629ff41fba9df96e
sbraunstein42 <sbraunstein@neit.edu>
2025-10-30 10:30:52 -0400

    Wrote comments in game loop (step 3)

--- challenges.js
+++ challenges.js
@@ -2,6 +2,13 @@ let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 
 function gameLoop() {
+    
+    //draw background
+    //draw stars
+    //move stars
+    //recycle stars/restart stars
+
+
 
 }
 
